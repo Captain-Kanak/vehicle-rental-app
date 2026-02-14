065637b6d57b1227995ffd2359380bc77c1068af
Captain-Kanak <kanakroy835@gmail.com>
2026-02-14 22:03:51 +0600

    navbar added

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function CommonLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      <main className="container mx-auto">{children}</main>
+    </>
+  );
+}
